1221c391db5fca8dbcf9aa148352a8196c6ccc40
oleh.kozyk <oleg.kozyk@beetroot.se>
2019-10-31 19:07:59 +0200

    fsdsewqe

--- core/base/controllers/RouteController.php
+++ core/base/controllers/RouteController.php
@@ -3,6 +3,8 @@
 
 namespace core\base\controllers;
 
+use core\base\settings\Settings;
+
 
 class RouteController
 {
@@ -13,8 +15,20 @@ class RouteController
         if (self::$_instance instanceof self) {
             return self::$_instance;
         }
-
         return self::$_instance = new self;
     }
+
+    private function __construct()
+    {
+        $s = Settings::get('routes'); 
+        exit();
+
+    }
+
+    private function __clone()
+    {
+    }
+
+
 
 }
--- index.php
+++ index.php
@@ -7,13 +7,15 @@ session_start();
 
 require_once 'config.php';
 require_once 'core/base/settings/internal_settings.php';
+require_once 'libraries/functions.php';
 
 use core\base\exceptions\RouteException;
 use core\base\controllers\RouteController;
 
 try {
 
-    RouteController::getInstance()->route();
+    //RouteController::getInstance()->route();
+    RouteController::getInstance();
 }
 
 catch(RouteException $e) {
